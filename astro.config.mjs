2d7ad92cafb0cf9ce6251be653b46db66382c74a
thebalayogesh <thebalayogesh@gmail.com>
2026-03-08 00:02:35 +0530

    Checking

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
+import cloudflare from '@astrojs/cloudflare';
 // import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   // "hybrid" is now the default behavior of "static"
-  output: 'static', 
+  output: 'static',
+
   // adapter: cloudflare(),
   integrations: [react(), markdoc(), keystatic()],
+
+  adapter: cloudflare(),
 });
--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,22 +1,23 @@
-// keystatic.config.ts
-import { config, fields, collection } from '@keystatic/core';
+import { config, fields, collection } from "@keystatic/core";
 
 export default config({
-  storage: process.env.NODE_ENV === 'development' 
-    ? { kind: 'local' } 
-    : { 
-        kind: 'github', 
-        repo: 'your-username/your-repo-name' 
+  storage: 
+  // import.meta.env.DEV
+    // ? { kind: "local" } // Uses your local hard drive when running 'bun dev'
+    // : 
+    {
+        kind: "github", // Uses GitHub API when live on Cloudflare
+        repo: "thebalayogesh/homelockersin",
       },
   collections: {
     posts: collection({
-      label: 'Posts',
-      slugField: 'title',
-      path: 'src/content/posts/*',
-      format: { contentField: 'content' },
+      label: "Posts",
+      slugField: "title",
+      path: "src/content/posts/*",
+      format: { contentField: "content" },
       schema: {
-        title: fields.slug({ name: { label: 'Title' } }),
-        content: fields.markdoc({ label: 'Content' }),
+        title: fields.slug({ name: { label: "Title" } }),
+        content: fields.markdoc({ label: "Content" }),
       },
     }),
   },
