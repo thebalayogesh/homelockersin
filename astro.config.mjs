26e98bcb725957e58757c212ef220b74b02f223c
thebalayogesh <thebalayogesh@gmail.com>
2026-03-08 00:14:03 +0530

    Add Cloudflare adapter and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,17 @@ import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 import cloudflare from '@astrojs/cloudflare';
-// import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  // "hybrid" is now the default behavior of "static"
-  output: 'static',
-
-  // adapter: cloudflare(),
-  integrations: [react(), markdoc(), keystatic()],
-
+  // Allows both static pages and the dynamic Keystatic API
+  output: 'static', 
+  
+  // Use the Cloudflare adapter for the Keystatic Admin API
   adapter: cloudflare(),
+
+  integrations: [
+    react(), 
+    markdoc(), 
+    keystatic()
+  ],
 });
